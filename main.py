d142a057b2a1481aa9e76e2ef7c7c1cd848ae523
Mengoreo <mengoreo@163.com>
2019-04-04 15:22:18 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import modules
+
+X = 0
+Y = 40
+WIDTH = 1200
+HEIGHT = 900
+while True:
+    original_screen = modules.capture(x=X, y=Y, width=WIDTH, height=HEIGHT)
+    processed_screen = modules.process(original_screen)
+    modules.show(processed_screen)
---
+++ modules.py
@@ -0,0 +1,22 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+from KeyFunctions import PressKey, ReleaseKey, KEYS
+
+
+def capture(x=0, y=0, width=13, height=14):
+    return np.array(ImageGrab.grab(bbox=(x, y, width, height)))
+
+
+def show(image):
+    cv2.show('Window', image)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destoryAllWindows()
+        break
+
+
+def process(image):
+    processed_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    return processed_image
