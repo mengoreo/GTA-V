ad7ecee28c5e1d3951b5c8790087b1c7bb3e53f3
mengoreo <mengoreo@163.com>
2019-05-29 09:12:54 +0800

    update main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import modules
 
+X = 10
+Y = 50
+WIDTH = 1190
+HEIGHT = 720
 
-X = 0
-Y = 40
-WIDTH = 1200
-HEIGHT = 900
+
 while True:
     original_screen = modules.capture(x=X, y=Y, width=WIDTH, height=HEIGHT)
     processed_screen = modules.process(original_screen)
